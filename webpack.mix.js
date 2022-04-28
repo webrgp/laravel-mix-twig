a213b6ad31ed15b9c9d706aa9224c425bdd9b9c2
Rodrigo Passos <rodrigopassos@mac.com>
2022-04-28 09:21:04 -0400

    add data test

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,8 @@ require('./src/index')
 mix.setPublicPath('tests/public')
 mix.version(['tests/public/test.css'])
 
-mix.twig('tests/src', 'tests/public')
+mix.twig('tests/src', 'tests/public', {
+  data: {
+    msg: 'Hello World'
+  }
+})
